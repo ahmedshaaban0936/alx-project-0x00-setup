87d48c5675efb1c6baa8b2783f1ace5935290696
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-12-18 16:41:10 +0200

    git Commit

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,6 +1,10 @@
-const Pill = () => {
-    return <div>Pill Component Placeholder</div>;
-  };
-  
-  export default Pill;
-  
+import { PillProps } from "@/interfaces";
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
+    </div>
+  )
+}
+
+export default Pill;
--- alx-project-0x00/pages/about.tsx
+++ alx-project-0x00/pages/about.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 const About: React.FC = () => {
-    return (
-      <div>
-        <h1 className="text-xl font-extralight">About Page</h1>
-      </div>
-    );
-  };
-  
-  export default About;
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">About Page</h1>
+    </div>
+  );
+};
+
+export default About;
   
